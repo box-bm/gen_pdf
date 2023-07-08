ab439f0b196190989a442e323993e74cd7996786
Brandon Manzo <brandong.manzog@icloud.com>
2023-07-08 12:10:54 -0600

    fixed errors

--- lib/screens/home/bills.dart
+++ lib/screens/home/bills.dart
@@ -2,10 +2,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gen_pdf/bloc/bills_bloc.dart';
 import 'package:gen_pdf/common.dart';
 import 'package:gen_pdf/cubit/form_cubit.dart';
-import 'package:gen_pdf/models/bill.dart';
 import 'package:gen_pdf/screens/new_bill.dart';
 import 'package:gen_pdf/screens/preview_pdf.dart';
-import 'package:gen_pdf/utils/gen_test_pdf.dart';
 import 'package:gen_pdf/widgets/bills_list.dart';
 
 class Bills extends StatefulWidget {
@@ -70,12 +68,6 @@ class _BillsState extends State<Bills> {
                                 },
                           icon: const Icon(FluentIcons.pdf),
                           label: const Text("Generar PDFs")),
-                      CommandBarButton(
-                          onPressed: () {
-                            genPDF(Bill(date: DateTime.now()));
-                          },
-                          icon: const Icon(FluentIcons.pdf),
-                          label: const Text("Pdf de prueba")),
                     ])),
             resizeToAvoidBottomInset: true,
             content: SafeArea(
