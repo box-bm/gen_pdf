bc9a4be03edfc0dd173cfd696046d15e06d910f5
Brandon Manzo <brandong.manzog@icloud.com>
2023-09-09 22:53:41 -0600

    change agreement appearance

--- lib/utils/document_templates/agreement.dart
+++ lib/utils/document_templates/agreement.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:gen_pdf/models/bill.dart';
+import 'package:gen_pdf/utils/document_templates/generics/header.dart';
 import 'package:gen_pdf/utils/document_templates/generics/signatures.dart';
 import 'package:intl/intl.dart';
 import 'package:pdf/pdf.dart';
@@ -12,21 +13,28 @@ Future<Page> getAgreementTemplate(Bill bill) async {
 
   var boldText = TextStyle(fontWeight: FontWeight.bold);
 
+  var headerMakanW = await headerMakan(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    mainAxisAlignment: MainAxisAlignment.center,
+    height: 100,
+  );
+
   var page = Page(
       pageFormat: PdfPageFormat.letter,
       margin: const EdgeInsets.fromLTRB(30, 30, 30, 20),
       theme: ThemeData(
           defaultTextStyle: TextStyle(
-              font: Font.helvetica(), lineSpacing: 0.5, fontSize: 9.3)),
+              font: Font.helvetica(), lineSpacing: 0.5, fontSize: 9.5)),
       build: (Context context) {
         return Stack(children: [
+          Align(alignment: Alignment.topLeft, child: headerMakanW),
           Expanded(
               child: Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 40),
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 32),
                   child: Column(
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
-                        Spacer(),
                         Text("CONTRATO DE VENTA".toUpperCase(),
                             textAlign: TextAlign.right,
                             style: boldText.copyWith(fontSize: 14)),
@@ -63,7 +71,7 @@ Future<Page> getAgreementTemplate(Bill bill) async {
                             style: boldText, textAlign: TextAlign.center),
                         SizedBox(height: 20),
                         Text(
-                            "CONTRATO DE VENTA en el que la empresa MAKAN GLOBAL SHIPPING. (el vendedor) Vende diversas mercancías a la empresa ${bill.consignerName.toUpperCase()}. (el comprador) Calle Elvira Méndez, edifico Interseco, piso B. Panamá, panamá. EL VENDEDOR y EL COMPRADOR, acuerdan conjuntamente llevar a cabo este contrato de acuerdo con las siguientes cláusulas, ACUERDO DE COMPRA DE MERCANCÍA MISCELÁNEA."),
+                            "CONTRATO DE VENTA en el que la empresa MAKAN GLOBAL SHIPPING. (el vendedor) Vende diversas mercancías a la empresa SERVYMAS, S.A. . (el comprador) Calle Elvira Méndez, edifico Interseco, piso B. Panamá, panamá. EL VENDEDOR y EL COMPRADOR, acuerdan conjuntamente llevar a cabo este contrato de acuerdo con las siguientes cláusulas, ACUERDO DE COMPRA DE MERCANCÍA MIS VARIAS."),
                         SizedBox(height: 8),
                         Text(
                             "PRIMERO: Ambas empresas están debidamente representadas, tienen plenos derechos y tienen el poder de celebrar contratos comerciales."),
@@ -79,33 +87,29 @@ Future<Page> getAgreementTemplate(Bill bill) async {
                         Text(
                             "5. El precio es libre y sin condiciones a que el vendedor se devuelva directa o indirectamente a cualquier parte de los ingresos de la reventa o a cualquier transferencia o uso posterior de los productos por parte del Comprador; no hay ningún tipo de pago de regalías, regalías o uso de mercancía."),
                         Text(
-                            "6. Ambas partes del contrato, comprador y vendedor, son entidades legales totalmente independientes y ninguno de sus empleados, administradores o propietarios tiene ningún vínculo, excepto el que está vinculado por la presente."),
+                            "6.	Ambas partes del contrato, comprador y vendedor, son entidades legales totalmente independientes y ninguno de sus empleados, administradores o propietarios tiene ningún vínculo."),
                         Text("7. No hay descuentos para el pronto pago."),
                         Text("8. Condiciones de entrega y pago:"),
                         Container(
-                            margin: const EdgeInsets.only(left: 36),
+                            margin: const EdgeInsets.only(left: 26),
                             child:
                                 Text("8.1. Punto de entrega: Puerto Quetzal")),
                         Container(
-                            margin: const EdgeInsets.only(left: 36),
+                            margin: const EdgeInsets.only(left: 26),
                             child: Text(
                                 "8.2. Pago: crédito de 90 días a partir de la fecha de la factura.")),
                         Text(
-                            "9. Las discrepancias en la recepción de la mercancía enviada y facturada correrán a cargo del comprador."),
+                            "9. Ambas partes Comprador reconoce el compromiso de pago por las mercancías recibidas y el Vendedor reconoce la responsabilidad de entrega de las mercancías en buen estado hasta el Puerto de Destino en Guatemala.."),
                         Text(
                             "10. El vendedor se encargará de cubrir los costos de exportación, de acuerdo con el incoterm CIF."),
                         SizedBox(height: 8),
                         Text(
-                            "SEGUNDO: Se establece el siguiente mecanismo de compra-venta: Cualquier controversia derivada de este contrato se transmitirá a través del Procedimiento."),
-                        SizedBox(height: 8),
-                        Text(
-                            "TERCERO: A este documento, adjunto los documentos originales, emitidos por MAKAN GLOBAL SHIPPING. que consisten en: Cotización de los productos; número ${bill.billNumber}, número de confirmación de compra ${bill.billNumber} del que se atestigua su legitimidad."),
+                            "SEGUNDO: Al presente documento, hago acompañar los documentos originales, emitidos por MAKAN GLOBAL SHIPPING. que consisten en: Cotización de los productos; número 50208030001, Factura número 50208030001de los cuales doy fé de su legitimidad."),
                         SizedBox(height: 8),
                         Text(
                             "Las partes declaran que aceptan todos y cada uno de los términos y condiciones establecidos en este contrato, con la emisión de la factura en el frente y el envío de la mercancía o el envío."),
                         Spacer(),
                         signatures(bill),
-                        Spacer(),
                       ]))),
           Watermark(
               fit: BoxFit.scaleDown,
--- lib/utils/document_templates/generics/header.dart
+++ lib/utils/document_templates/generics/header.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/services.dart';
 import 'package:pdf/widgets.dart';
 
-Future<Widget> headerMakan() async {
+Future<Widget> headerMakan({
+  CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
+  MainAxisAlignment mainAxisAlignment = MainAxisAlignment.end,
+  double height = 90,
+}) async {
   var logo = MemoryImage(
     (await rootBundle.load('assets/img/logo.png')).buffer.asUint8List(),
   );
 
   return Row(children: [
     Container(
-        height: 90,
+        height: height,
         width: 90,
-        child: Center(child: Image(logo, width: 80, height: 80))),
+        child: Center(child: Image(logo, width: 80, height: height - 10))),
     Expanded(
         child: Container(
             padding: const EdgeInsets.all(4),
-            height: 90,
+            height: height,
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: mainAxisAlignment,
+                crossAxisAlignment: crossAxisAlignment,
                 children: [
                   Text("MAKAN GLOBAL SHIPPING".toUpperCase(),
-                      style: TextStyle(font: Font.timesBold(), fontSize: 20)),
-                  Text(
-                      "Calle Elvira Méndez, edificio Interseco, Piso B.Panamá, Panamá.")
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                  Text("Calle Elvira Méndez No. 10 último piso"),
+                  Text("Panamá, República de Panamá ")
                 ])))
   ]);
 }
